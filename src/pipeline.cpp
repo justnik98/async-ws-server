5971b347cfcdd45dc3130b4b9da84aa6600ff8f8
Nikita Yankovskii <yannikita98@gmail.com>
2021-03-06 19:20:14 +0300

    added bash script call to pipeline

--- src/pipeline.cpp
+++ src/pipeline.cpp
@@ -1,8 +1,9 @@
 //
 // Created by justnik on 06.03.2021.
 //
-//#include "json.hpp"
+//
 
+#include <iostream>
 #include <fstream>
 #include <nlohmann/json.hpp>
 #include <sstream>
@@ -14,20 +15,33 @@ using json = nlohmann::json;
 void runPipeline(std::string &str) {
     std::string s = R"({"UID": 0,"language": "cpp","version": 17,"taskUID": 1,"program": "int main(){return 0;}"})"; //mock
     json n = json::parse(s);
-    std::string res = n["program"];
+    std::string code = n["program"];
+
+    if (code.starts_with('\"')) {
+        code.erase(str.begin());
+    }
+    if (code.ends_with('\"')) {
+        code.erase(str.end() - 1);
+    }
+
+    //forming the file name
     std::stringstream ss;
-    if (res.starts_with('\"')) {
-        res.erase(str.begin());
-    }
-    if (res.ends_with('\"')) {
-        res.erase(str.end() - 1);
-    }
-    ss << n["UID"] << "_" << n["taskUID"] << ".";
+    ss << "files/" << n["UID"] << "_" << n["taskUID"] << ".";
     std::string lang = n["language"];
     lang.erase(remove_if(lang.begin(), lang.end(), [](unsigned char x) { return x == '\"'; }),
                lang.end());
-    ss << lang;
     std::string filename = ss.str();
-    std::ofstream out(filename);
-    out << res << std::endl;
+    std::ofstream out(filename + lang);
+    out << code << std::endl;
+    out.close();
+
+    system("./script.sh");//mock
+    std::ifstream in(filename + "res");
+    if (!in.is_open()) {
+        std::cerr << "Time exceeded";
+        return;
+    }
+    std::string res;
+    in >> res;
+    std::cout << res;
 }
